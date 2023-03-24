601ab83d5822b4565c060d4a0187b140046827df
Meruem117 <1572677319@qq.com>
2023-03-24 23:45:37 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,48 @@ import { Box, TextField, Button } from '@mui/material'
 import './App.css'
 
 function App() {
-    const [html, setHtml] = useState<string>('')
-    // const [body, setBody] = useState<string>('')
+    const [code, setCode] = useState<string>('')
+    const [str, setStr] = useState<string>('')
 
     const inputChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
-        setHtml(event.target.value)
+        setCode(event.target.value)
     }
 
     const submit = (): void => {
-        console.log(html)
+        console.log(code)
     }
 
-    const convertHtml = (html: string): string[] => {
+    const convertCodeToBody = (code: string): string[] => {
         let body: string[] = []
         return body
     }
 
     return (
         <div className="App">
-            <Box sx={{ p: 5 }}>
+            <Box sx={{ p: 5, display: 'flex', gap: 5 }}>
                 <TextField
-                    id='html-text'
-                    label='Html'
+                    id='code-text'
+                    label='Code'
                     fullWidth
-                    value={html}
+                    value={code}
                     inputProps={{
                         autoComplete: 'off',
                         color: 'primary',
                         onChange: inputChange
                     }}
                 />
-                <Button sx={{ mt: 2 }} variant="outlined" onClick={submit}>Submit</Button>
+                <Button sx={{ mt: 2 }} onClick={submit}>Submit</Button>
+                <TextField
+                    id='str-text'
+                    label='Str'
+                    fullWidth
+                    value={str}
+                    inputProps={{
+                        autoComplete: 'off',
+                        color: 'primary',
+                        onChange: inputChange
+                    }}
+                />
             </Box>
         </div>
     )
