3c8fe2e77fdcb430916d1fa5f771d0ae6222ff25
Meruem117 <1572677319@qq.com>
2023-03-26 23:07:45 +0800

    get blocks

--- src/pages/snippet/index.tsx
+++ src/pages/snippet/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Editor, EditorState } from 'draft-js'
+import { Editor, EditorState, convertToRaw } from 'draft-js'
 import 'draft-js/dist/Draft.css'
+import { Button } from '@mui/material'
 import style from './index.module.css'
 
 const Snippet: React.FC = () => {
@@ -8,9 +9,17 @@ const Snippet: React.FC = () => {
         EditorState.createEmpty()
     )
 
+    const submit = (): void => {
+        let blocks = convertToRaw(editorState.getCurrentContent())
+        console.log(blocks)
+    }
+
     return (
         <div className={style.editor}>
             <Editor editorState={editorState} onChange={setEditorState} />
+            <div className={style.button}>
+                <Button variant="outlined" onClick={submit}>Submit</Button>
+            </div>
         </div>
     )
 }
