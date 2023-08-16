0f5780edb70196b9d10bd9a7cc011a336e10d2ff
Meruem117 <1572677319@qq.com>
2023-08-16 21:56:50 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import React, { lazy } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Box, Link } from '@mui/material'
-
-const Snippet = lazy(() => import('./pages/snippet'))
+import React from 'react'
+import Index from './pages'
 
 function App(): React.ReactElement {
     return (
-        <Box sx={{
-            padding: '30px 50px',
-            display: 'flex',
-            justifyContent: 'space-between'
-        }}>
-            <Box sx={{
-                width: '12%',
-                display: 'flex',
-                flexDirection: 'column',
-                borderRight: '1px dashed gray'
-            }}>
-                <Link href='snippet'>snippet</Link>
-            </Box>
-            <Box sx={{
-                width: '85%'
-            }}>
-                <Routes>
-                    <Route path="snippet" element={<Snippet />} />
-                </Routes>
-            </Box>
-        </Box>
+        <Index />
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Box, Link } from '@mui/material'
+
+const Snippet = lazy(() => import('./snippet'))
 
 const Index: React.FC = () => {
     return (
-        <div>Index</div>
+        <Box sx={{
+            padding: '30px 50px',
+            display: 'flex',
+            justifyContent: 'space-between'
+        }}>
+            <Box sx={{
+                width: '12%',
+                display: 'flex',
+                flexDirection: 'column',
+                borderRight: '1px dashed gray'
+            }}>
+                <Link href='snippet'>snippet</Link>
+            </Box>
+            <Box sx={{
+                width: '85%'
+            }}>
+                <Routes>
+                    <Route path="snippet" element={<Snippet />} />
+                </Routes>
+            </Box>
+        </Box>
     )
 }
 
