10f05e4ba65c14d9292157fa9f18af8cbc8773cb
Meruem117 <1572677319@qq.com>
2023-09-11 21:38:08 +0800

    add editor

--- src/pages/sort/index.tsx
+++ src/pages/sort/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Editor, EditorState, convertToRaw } from 'draft-js'
+import 'draft-js/dist/Draft.css'
 import style from './index.module.css'
 
 const Sort: React.FC = () => {
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
+    )
+
     return (
         <>
-            <div className={style.container}></div>
+            <div className={style.editor}>
+                <Editor editorState={editorState} onChange={setEditorState} />
+            </div>
         </>
     )
 }
