23d6ead42f12f5c78676e60897e507e3a09d533e
Meruem117 <1572677319@qq.com>
2023-09-12 21:01:24 +0800

    update

--- src/pages/sort/index.tsx
+++ src/pages/sort/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Button } from '@mui/material'
 import { Editor, EditorState, convertToRaw } from 'draft-js'
 import 'draft-js/dist/Draft.css'
 import style from './index.module.css'
@@ -8,10 +10,22 @@ const Sort: React.FC = () => {
         EditorState.createEmpty()
     )
 
+    const copy = (): string => {
+        const { blocks } = convertToRaw(editorState.getCurrentContent())
+        let body = ''
+        let blockList = blocks.map(block => block.text)
+        return body
+    }
+
     return (
         <>
             <div className={style.editor}>
                 <Editor editorState={editorState} onChange={setEditorState} />
+            </div>
+            <div className={style.button}>
+                <CopyToClipboard text={copy()}>
+                    <Button variant="outlined">Copy</Button>
+                </CopyToClipboard>
             </div>
         </>
     )
